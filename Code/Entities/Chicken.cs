0280afe92b30e404c736be6e7a51439965cf4ccf
mc0239 <martin.cebular@gmail.com>
2021-01-29 22:19:39 +0100

    Chicken Off-Track Movement (so they don't move in straight lines)

--- Code/Entities/Chicken.cs
+++ Code/Entities/Chicken.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Diagnostics;
 
 namespace UglyDuckling.Code.Entities
 {
 	class Chicken : Entity
 	{
+		private const double OFF_TRACK_CHANCE = 0.50;
+		// Putting off-track deviation over 0.5 produces weird behaviour.
+		private const double OFF_TRACK_DEVIATION = 0.40;
 
-		private float moveSpeed = 4f; // default
+		private Random random = new Random();
+
+		public float MoveSpeed { get; set; } = 4f; // default
 		public Vector2 TargetPosition { get; set; }
+
+		private Vector2? OffTrackDirection = null;
+		private double OffTrackTime = 0;
 
 		public Chicken() : base(Vector2.Zero)
 		{
@@ -24,14 +33,35 @@ namespace UglyDuckling.Code.Entities
 			double errorDist = Vector2.Distance(GetPosition(), TargetPosition);
 			if (errorDist > 2)
             {
-				// move towards the target with at the moveSpeed rate
-				Vector2 moveTowards = Vector2.Normalize(Vector2.Subtract(TargetPosition, GetPosition()));
-				moveTowards *= moveSpeed;
-				Move(moveTowards);
-				// Debug.WriteLine("Target: " + TargetPosition);
+				MoveTowardsTarget(gameTime);
             }
         }
 
-		//
+		private void MoveTowardsTarget(GameTime gameTime)
+        {
+			Vector2 moveTowards = Vector2.Normalize(Vector2.Subtract(TargetPosition, GetPosition()));
+			if (OffTrackDirection != null)
+			{
+				// off-track direction is added after normalization - thus affecting movement speed as well
+				// (i.e. when off-tracking, chickens can go slower/faster, depends on deviation).
+				moveTowards += (Vector2)OffTrackDirection;
+				OffTrackTime += gameTime.ElapsedGameTime.TotalSeconds;
+
+				if (OffTrackTime > 2)
+				{
+					OffTrackDirection = null;
+					OffTrackTime = 0;
+				}
+			}
+			else if (OffTrackDirection == null && random.NextDouble() < OFF_TRACK_CHANCE)
+			{
+				float dx = (float)(random.NextDouble() * (OFF_TRACK_DEVIATION * 2) - OFF_TRACK_DEVIATION);
+				float dy = (float)(random.NextDouble() * (OFF_TRACK_DEVIATION * 2) - OFF_TRACK_DEVIATION);
+				OffTrackDirection = new Vector2(dx, dy);
+			}
+
+			moveTowards *= MoveSpeed;
+			Move(moveTowards);
+		}
 	}
 }
